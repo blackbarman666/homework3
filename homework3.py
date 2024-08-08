92b5b7400c17a684118dff68beef7e3d703a1d59
blackbarman666 <mihaman05@gmail.com>
2024-08-08 22:45:14 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,5 @@
+first_name= "домашка12"
+sekond_name= "время для домашки1.5"
+third_name= "курс по Python"
+fourth_name= "время на одно задание 0.125"
+print(third_name,first_name,sekond_name,fourth_name)
